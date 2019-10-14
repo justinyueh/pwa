d42055462cafa4be39c2c9483391880819836d23
yufeng <yufeng@fangdd.com>
2019-10-14 12:00:10 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const express = require('express');
+const ejs = require('ejs');
+const favicon = require('serve-favicon');
+
+const app = express();
+const port = 3000;
+
+app.set('x-powered-by', false);
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(express.static('public'));
+
+app.set('views', './views');
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Hey', message: 'Hello there!' });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
